ade0a9e1bf3c52a7c907efcd8bb92662605a758b
daskain <daskain@gmail.com>
2019-02-09 21:07:37 +0300

    Update ParseResp.java

--- src/tools/ParseResp.java
+++ src/tools/ParseResp.java
@@ -12,7 +12,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class ParseResp
  */
-@WebServlet("/ParseResp")
+@WebServlet("/paresp")
 public class ParseResp extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
